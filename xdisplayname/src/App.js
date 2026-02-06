8d5ad913240b99e7593e60659d444d5bcbe88f3a
nehal singh <nehalsingh903@gmail.com>
2026-02-06 23:56:32 +0530

    Update App UI

--- xdisplayname/src/App.js
+++ xdisplayname/src/App.js
@@ -17,6 +17,7 @@ function App() {
 
   return (
     <div>
+      <h1>Full Name Display</h1>
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="firstName">First Name:</label>
