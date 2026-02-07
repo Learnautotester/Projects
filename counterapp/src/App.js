96a7012b9e1a901d5620a7fce10abf6fc6bd58dd
nehal singh <nehalsingh903@gmail.com>
2026-02-07 14:29:01 +0530

    Add counterapp React project

---
+++ counterapp/src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+
+   const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h2>Counter App</h2>
+      <p>Count: {count}</p>
+
+      <button onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+
+      <button onClick={() => setCount(count - 1)}>
+        Decrement
+      </button>
+    </div>
+    
+  );
+}
+
+export default App;
