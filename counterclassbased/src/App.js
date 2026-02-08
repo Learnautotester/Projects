a9a48d6bf5de6a18fbeb4a1d02ebb16406577ca6
nehal singh <nehalsingh903@gmail.com>
2026-02-08 14:27:14 +0530

    CounterApp class based component

---
+++ counterclassbased/src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+
+    // Initial state
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  };
+
+  render() {
+    return (
+      
+      <div>
+         <h1>Counter App</h1>
+        <p>Count: {this.state.count}</p>
+
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    );
+  }
+}
+
+export default App;
