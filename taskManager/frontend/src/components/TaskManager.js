9351afa5db5b3e2087b68326b3275477cc94faa1
nehal singh <nehalsingh903@gmail.com>
2026-07-26 13:21:29 +0530

    updated frontend url

--- taskManager/frontend/src/components/TaskManager.js
+++ taskManager/frontend/src/components/TaskManager.js
@@ -17,7 +17,7 @@ export const TaskManager = () => {
   useEffect(() => {
     const fetchTasks = async () => {
       try {
-        const response = await axios.get("http://localhost:8082/tasks");
+        const response = await axios.get("https://taskmanager-6d8l.onrender.com/tasks");
         setTasks(response.data);
       } catch (err) {
         console.error("Error fetching tasks:", err);
@@ -59,16 +59,16 @@ export const TaskManager = () => {
 
     try {
       if (isEditing) {
-        await axios.patch(`http://localhost:8082/tasks/${taskData._id}`, {
+        await axios.patch(`https://taskmanager-6d8l.onrender.com/${taskData._id}`, {
           title: taskData.title,
           description: taskData.description,
           deadline: taskData.deadline,
         });
       } else {
         console.log("formData in handleSave", formData);
-        await axios.post("http://localhost:8082/tasks", formData);
+        await axios.post("https://taskmanager-6d8l.onrender.com/tasks", formData);
       }
-      const response = await axios.get("http://localhost:8082/tasks");
+      const response = await axios.get("https://taskmanager-6d8l.onrender.com/tasks");
       setTasks(response.data);
       handleClose();
     } catch (err) {
@@ -87,10 +87,10 @@ export const TaskManager = () => {
 
   const handleMarkAsDone = async (taskId) => {
     try {
-      await axios.patch(`http://localhost:8082/tasks/${taskId}`, {
+      await axios.patch(`https://taskmanager-6d8l.onrender.com/tasks/${taskId}`, {
         status: "DONE",
       });
-      const response = await axios.get("http://localhost:8082/tasks");
+      const response = await axios.get("https://taskmanager-6d8l.onrender.com/tasks");
       setTasks(response.data);
     } catch (err) {
       console.error("Error updating task:", err);
@@ -112,8 +112,8 @@ export const TaskManager = () => {
   const handleDelete = async (taskId) => {
     if (window.confirm("Are you sure you want to delete this task?")) {
       try {
-        await axios.delete(`http://localhost:8082/tasks/${taskId}`);
-        const response = await axios.get("http://localhost:8082/tasks");
+        await axios.delete(`https://taskmanager-6d8l.onrender.com/tasks/${taskId}`);
+        const response = await axios.get("https://taskmanager-6d8l.onrender.com/tasks");
         setTasks(response.data);
       } catch (err) {
         console.error("Error deleting task:", err);
